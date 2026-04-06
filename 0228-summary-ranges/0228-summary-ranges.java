0401370adc1d17033b68d225ad07c4c33e40944d
Abhinav Kusampudi <140812124+Harshabhi6129@users.noreply.github.com>
2026-04-06 13:11:23 -0400

    Time: 3 ms (79.67%), Space: 43.3 MB (28.73%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int start = nums[i];
+
+            // extend the range while consecutive
+            while (i + 1 < n && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+
+            int end = nums[i];
+
+            if (start == end) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + end);
+            }
+        }
+
+        return result;
+    }
+}
