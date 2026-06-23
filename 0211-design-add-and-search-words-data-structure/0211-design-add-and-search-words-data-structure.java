014446c557810e5629691849914e21b809803410
Abhinav Kusampudi <140812124+Harshabhi6129@users.noreply.github.com>
2026-06-22 20:16:27 -0400

    Time: 195 ms (59.23%), Space: 274.2 MB (60.98%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,56 @@
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isWord;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.isWord = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int index, TrieNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        if (index == word.length()) {
+            return node.isWord;
+        }
+
+        char ch = word.charAt(index);
+
+        if (ch == '.') {
+            for (TrieNode child : node.children) {
+                if (dfs(word, index + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        return dfs(word, index + 1, node.children[ch - 'a']);
+    }
+}
