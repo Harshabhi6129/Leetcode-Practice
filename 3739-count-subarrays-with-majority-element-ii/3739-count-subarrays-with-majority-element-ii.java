e7cc9290b70883c165a60226dd53e9cac846f486
Abhinav Kusampudi <140812124+Harshabhi6129@users.noreply.github.com>
2026-06-26 10:06:55 -0400

    Time: 14 ms (67.21%), Space: 87.8 MB (81.97%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    class Fenwick {
+        int[] bit;
+
+        Fenwick(int n) {
+            bit = new int[n + 2];
+        }
+
+        void update(int idx, int val) {
+            while (idx < bit.length) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+
+        Fenwick ft = new Fenwick(2 * n + 5);
+
+        int offset = n + 2;
+        int prefix = 0;
+        long ans = 0;
+
+        ft.update(offset, 1); // prefix sum = 0
+
+        for (int num : nums) {
+            if (num == target) {
+                prefix++;
+            } else {
+                prefix--;
+            }
+
+            int idx = prefix + offset;
+
+            ans += ft.query(idx - 1);
+
+            ft.update(idx, 1);
+        }
+
+        return ans;
+    }
+}
